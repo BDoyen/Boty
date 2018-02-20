a6ecc9cf76e4911869d33151bbebb818319d11bc
BDoyen <baptiste.doyen@supelec.fr>
2018-02-20 15:56:23 +0100

    make it better

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ var connector = new builder.ChatConnector({
  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+
 //bot object
 var bot = new builder.UniversalBot(connector)
      .set('storage', cosmosStorage);
@@ -35,7 +36,7 @@ var bot = new builder.UniversalBot(connector)
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
